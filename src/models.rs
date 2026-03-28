226f34768ab42bcab1b28540c212ea2dfa122926
Thijs Kamphuis <thijskamphuis@icloud.com>
2026-03-28 02:13:32 +0100

    nodfes

--- src/models.rs
+++ src/models.rs
@@ -148,7 +148,7 @@ pub struct Node {
 pub struct ManusData {
     #[serde(rename = "departments")]
     pub departments: HashMap<u32, Department>,
-
+qqqqqqqqqqqqqqqqq
     #[serde(rename = "nodes")]
     pub nodes: HashMap<u32, Node>,
 
@@ -185,8 +185,8 @@ impl ManusData {
 
                 event.push(Location::new(format!(
                     "{} - {}",
-                    entry.nodes.get(&entry.node_id.parse::<u32>().unwrap()).unwrap().code.clone(),
-                    entry.nodes.get(&entry.node_id.parse::<u32>().unwrap()).unwrap().name.clone(),
+                    schedule.nodes.get(&entry.node_id.parse::<u32>().unwrap()).unwrap().code.clone(),
+                    schedule.nodes.get(&entry.node_id.parse::<u32>().unwrap()).unwrap().name.clone(),
                 )));
 
                 events.push(event);
